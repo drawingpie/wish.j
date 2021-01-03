869fcc2a654d9a49685ea209d544d1f63ecd56e6
drawingpie <74900663+drawingpie@users.noreply.github.com>
2021-01-03 21:47:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1184,3 +1184,23 @@ var Application = function() {
 }
 
 window.application = new Application();
+
+//슬라이드메뉴
+$(document).ready(function(){
+
+        $('.menu_btn>a').on('click', function(){
+            $('.menu_bg').show();
+            $('.sidebar_menu').show().animate({
+                right:0
+            });
+        });
+        $('.close_btn>a').on('click', function(){
+            $('.menu_bg').hide();
+            $('.sidebar_menu').animate({
+                right: '-' + 50 + '%'
+                       },function(){
+$('.sidebar_menu').hide();
+});
+        });
+
+    });

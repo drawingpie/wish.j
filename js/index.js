af72118da939b7fa9369581fad8d380d374016c5
drawingpie <74900663+drawingpie@users.noreply.github.com>
2021-05-28 18:39:07 +0900

    052802

--- js/index.js
+++ js/index.js
@@ -1,15 +1,36 @@
 //슬라이드메뉴
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+	document.getElementById("myNav").style.width = "100%";
 }
 
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+	document.getElementById("myNav").style.width = "0%";
 }
 
 //모바일 세로모드고정
-screen.addEventListener("orientationchange", function () {
-  console.log("The orientation of the screen is: " + screen.orientation);
-});
+// $(window).on("orientationchange", function(event) {
+// 	if (window.matchMedia("(orientation: portrait)").matches) {
+// 		// 세로 모드 (평소 사용하는 각도)
+// 		console.log("가로금지");
+// 		$("#orientation_val").html("portrait");
+// 	} else if (window.matchMedia("(orientation: landscape)").matches) {
+// 		// 가로 모드 (동영상 볼때 사용하는 각도)
+// 		console.log("가로금지");
+// 		$("#orientation_val").html("landscape");
+// 	} else {
+// 		alert("The orientation has changed!");
+// 	}
+// });
 
-screen.lockOrientation('landscape');
+
+$(window).on("orientationchange", function(event) {
+	if (window.matchMedia("(orientation: portrait)").matches) {
+		// 세로 모드 (평소 사용하는 각도)
+		alert("세로로 보세요");
+    $(window).off("(orientation: portrait)");
+	} else if (window.matchMedia("(orientation: landscape)").matches) {
+		// 가로 모드 (동영상 볼때 사용하는 각도)
+		console.log("가로금지");
+		
+	}
+});

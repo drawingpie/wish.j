3bc4d16d5b41be6e17b0b81512e6c6a1797ba2c4
drawingpie <74900663+drawingpie@users.noreply.github.com>
2021-05-28 19:16:33 +0900

    04

--- js/index.js
+++ js/index.js
@@ -8,28 +8,12 @@ function closeNav() {
 }
 
 //모바일 세로모드고정
-// $(window).on("orientationchange", function(event) {
-// 	if (window.matchMedia("(orientation: portrait)").matches) {
-// 		// 세로 모드 (평소 사용하는 각도)
-// 		console.log("가로금지");
-// 		$("#orientation_val").html("portrait");
-// 	} else if (window.matchMedia("(orientation: landscape)").matches) {
-// 		// 가로 모드 (동영상 볼때 사용하는 각도)
-// 		console.log("가로금지");
-// 		$("#orientation_val").html("landscape");
-// 	} else {
-// 		alert("The orientation has changed!");
-// 	}
-// });
-
-
 $(window).on("orientationchange", function(event) {
 	if (window.matchMedia("(orientation: portrait)").matches) {
-		// 세로 모드 (평소 사용하는 각도)
+		// 가로로 볼 때 경고창
 		alert("가로는 No No~ 세로로 보시는게 좋아요");
-    $("body").rotate("180deg");
 	} else if (window.matchMedia("(orientation: landscape)").matches) {
-		// 가로 모드 (동영상 볼때 사용하는 각도)
+		// 세로모드고정
 		console.log("가로금지");
 	}
 });

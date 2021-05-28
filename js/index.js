89ab2b84a4ed10fa66c009ef3d277075fcba7dfc
drawingpie <74900663+drawingpie@users.noreply.github.com>
2021-05-28 17:15:37 +0900

    01

--- js/index.js
+++ js/index.js
@@ -6,3 +6,10 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+//모바일 세로모드고정
+screen.addEventListener("orientationchange", function () {
+  console.log("The orientation of the screen is: " + screen.orientation);
+});
+
+screen.lockOrientation('landscape');
